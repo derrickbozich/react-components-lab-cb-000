40972d62346ec5bb7f07d9896e085e47d6567fe8
derrick b <dbozich1@gmail.com>
2018-10-21 01:22:17 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -61,7 +61,9 @@ export class App extends Component {
   render() {
     return (
       <div id="app">
-
+        < OlderCoaster />
+        < InFrontOfYou />
+        < ButcherShop />
       </div>
     )
   }
