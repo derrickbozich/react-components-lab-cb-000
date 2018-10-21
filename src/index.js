d1b72a47cf51c6d451471fdea0c8ec9fc045bfe9
derrick b <dbozich1@gmail.com>
2018-10-21 01:07:15 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ const BUTCHER_PRODUCTS = [
 export class OlderCoaster extends Component {
 
   // this function should return the JSX we want the DOM to render
-  // render() {
-  //   return (
-  //     <div>Hello!</div>
-  //   )
-  // }
-  //
+  render() {
+    return (
+      <div>Hello!</div>
+    )
+  }
+  
 }
 
 export class InFrontOfYou extends Component {
